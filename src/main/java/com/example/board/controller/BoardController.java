a24ea2b229e21c77745665100d1265a7d57d2dcf
sakura <chou.creme.294@gmail.com>
2023-09-21 15:35:24 +0900

    20. 掲示板を作成する - 3終了時 #3

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -18,6 +18,8 @@ import java.util.Optional;
 
 import org.springframework.validation.annotation.Validated;
 
+import com.example.board.validation.GroupOrder;
+
 /**
  * 掲示板のフロントコントローラー.
  */
@@ -49,7 +51,7 @@ public class BoardController {
     * @return 一覧を設定したモデル
     */
     private Model setList(Model model) {
-    	Iterable<Post> list = repository.findAll();
+    	Iterable<Post> list = repository.findByDeletedFalseOrderByUpdatedDateDesc();
     	model.addAttribute("list", list);
     	return model;
      }
@@ -62,7 +64,7 @@ public class BoardController {
      */
      @RequestMapping(value = "/create", method = RequestMethod.POST)
      //public String create(@ModelAttribute("form") Post form, BindingResult result,Model model) {
-     public String create(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+     public String create(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result, Model model) {
     	if (!result.hasErrors()) {
     		repository.saveAndFlush(PostFactory.createPost(form));
     		model.addAttribute("form", PostFactory.newPost());
@@ -97,7 +99,7 @@ public class BoardController {
       */
       @RequestMapping(value = "/update", method = RequestMethod.POST)
       //public String update(@ModelAttribute("form") Post form, Model model) {
-      public String update(@ModelAttribute("form") @Validated Post form, BindingResult result, Model model) {
+      public String update(@ModelAttribute("form") @Validated(GroupOrder.class) Post form, BindingResult result, Model model) {
     	  if (!result.hasErrors()) {
     		  Optional<Post> post = repository.findById(form.getId());
     		  repository.saveAndFlush(PostFactory.updatePost(post.get(), form));
